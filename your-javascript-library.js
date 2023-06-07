a9b0f525da6b57986c05ea879c6d1b7afa7efb95
alirezza <46361013+alirezza@users.noreply.github.com>
2023-06-07 16:31:06 +0200

    Create your-javascript-library.js

---
+++ your-javascript-library.js
@@ -0,0 +1,10 @@
+// JS-Datei: your-javascript-library.js
+// Diese Datei enthält die JavaScript-Bibliothek (z.B. ScrollReveal.js)
+
+// Beispiel mit ScrollReveal.js
+ScrollReveal().reveal('.scroll-reveal', {
+  delay: 200,
+  distance: '50px',
+  easing: 'ease-in-out',
+  origin: 'bottom',
+});
