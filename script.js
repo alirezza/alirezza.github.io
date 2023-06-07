86af7631860bd647c175fb25232e7272371cbeb9
alirezza <46361013+alirezza@users.noreply.github.com>
2023-06-07 16:30:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Nothing to add in this basic example, but you can add your own JavaScript code if needed
+// JS-Datei: your-javascript-file.js
+// Diese Datei enthält das JavaScript für die interaktiven Funktionen der Webseite
+
+// Menü-Animation
+function toggleMenu() {
+  var menu = document.getElementById('nav-menu');
+  menu.classList.toggle('show');
+}
+
+// Animationsübergänge
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.scroll-reveal');
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+// Weitere JavaScript-Funktionen für die Lightbox-Galerie, Formularüberprüfung usw. hier einfügen
